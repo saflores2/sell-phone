c8059fe5395bf22de47aa01d625611acf47d9099
sofia flores <saflores2@uc.cl>
2023-03-01 04:10:36 -0300

    acptar y rechazar solicitudes de compra

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -1,4 +1,6 @@
 class RegistersController < ApplicationController
+  before_action :skip_authorization
+
   def create
     @register = Register.new
     @register.user = current_user
@@ -7,14 +9,14 @@ class RegistersController < ApplicationController
     redirect_to payment_register_path(@register.id) if @register.save
   end
 
-  def update
-    @register = Register.find(params[:id])
-    @register.status = "comprado"
-    redirect_to register_path(@register)
-  end
+  # def update
+  #   @register = Register.find(params[:id])
+  #   @register.status = "comprado"
+  #   redirect_to register_path(@register)
+  # end
 
-  def sell_response
-  end
+  # def sell_response
+  # end
 
   def payment
   end
@@ -23,6 +25,26 @@ class RegistersController < ApplicationController
   end
 
   def my_registers
-    @registers = Register.where(@register.user = user_id)
+    @registers = Register.where(user_id: current_user.id)#, status: "solicitado")
+  end
+
+  def accept
+    @register = Register.find(params[:id])
+    product_id = @register.product.id
+    other_registers = Register.where(product_id: product_id)
+    other_registers.each do |reg|
+      reg.status = "rechazado"
+      reg.save
+    end
+    @register.status = "aceptado"
+    @register.save
+    redirect_to my_registers_registers_path
+  end
+
+  def reject
+    @register = Register.find(params[:id])
+    @register.status = "rechazado"
+    @register.save
+    redirect_to my_registers_registers_path
   end
 end
