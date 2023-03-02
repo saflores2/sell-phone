b354525b01816729e3a0b0ee3c0fbb789a4b62c2
sofia flores <saflores2@uc.cl>
2023-03-02 16:13:23 -0300

    vistas de index, my_products y show para products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :register
+  has_many :registers, dependent: :destroy
   has_many_attached :photos
   validates :name, :year, :brand, :model, :status, :price, presence: true
   validates_inclusion_of :status, in: ["Nuevo", "Usado"]
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+  against: [ :name, :model, :brand, :operator ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
